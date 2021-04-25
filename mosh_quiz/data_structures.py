d98fc0d3190a8fc5ea185719a9f8b15c2e6a44f5
Albert <ishengomaalbert@gmail.com>
2021-04-25 19:48:13 +0300

    Solved codewars & some quiz qn

---
+++ mosh_quiz/data_structures.py
@@ -0,0 +1,15 @@
+# Find the character with most occurences in sentence
+sentence = "This is a common interview question"
+
+char_frequency = {}
+
+for char in sentence:
+    if char in char_frequency:
+        char_frequency[char] += 1
+    else:
+        char_frequency[char] = 1
+
+sorted_char_frequency = sorted(char_frequency.items(),
+                               key=lambda kv: kv[1],
+                               reverse=True)
+print(sorted_char_frequency[0])
